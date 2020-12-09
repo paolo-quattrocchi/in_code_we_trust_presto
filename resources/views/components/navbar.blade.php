9d91c2b7ab86c4ccb2ac36a70a536035cfff55bd
valeria vallese <you@example.com>
2020-12-09 17:55:11 +0100

    modifiche css navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light shadow">
+<nav class="navbar navbar-expand-lg bg-nav shadow">
     <div class="container-fluid">
       <a class="navbar-brand" href="/"></a>
       
@@ -11,25 +11,25 @@
             <span class="navbar-toggler-icon"></span>
           </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav ">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Home
+            <a class="nav-link text-white " href="/">Home
                   <span class="sr-only">(current)</span>
                 </a>
           </li>
           <li class="nav-item">
-          <a class="nav-link" href="{{route('posts.index')}}">Vedi gli annunci</a>
+          <a class="nav-link text-white" href="{{route('posts.index')}}">Vedi gli annunci</a>
           </li>
           <li class="nav-item">
-          <a class="nav-link" href="{{route('posts.create')}}">Pubblica annuncio</a>
+          <a class="nav-link text-white" href="{{route('posts.create')}}">Pubblica annuncio</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Categorie
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               @foreach ($categories as $category)
-              <a class="dropdown-item" href="{{ route('categories.show', compact('category'))}}">{{$category->name}}</a>
+              <a class="dropdown-item " href="{{ route('categories.show', compact('category'))}}">{{$category->name}}</a>
               
               
               @endforeach
@@ -45,13 +45,13 @@
             @guest
                 @if (Route::has('login'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                 @endif
                 
                 @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                        <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registrati') }}</a>
                     </li>
                 @endif
             @else
@@ -74,26 +74,9 @@
                 </li>
 
                 @endguest
-                <a href="{{route('posts.create')}}" class="btn btno btn-primary ml-3 rounded-pill text-dark">Inserisci annuncio</a>
+                <a href="{{route('posts.create')}}" class="btn bg-btn  ml-3 rounded-pill text-white">Inserisci annuncio</a>
         </ul>
       </div>
     </div>
   </nav>
-  <style>
-    .bg-nav{
-      color:rgb(07, 11,134);
-    }
-
-    .btno{
-     background:rgb(214, 218, 10);
-     
-    }
-  
-    .btno:hover {
-    background-color: green;
-    text-decoration: none;
-  }
-  nav{
-    background: rgb(0, 148, 221);
-  }
-    </style>
+ 
